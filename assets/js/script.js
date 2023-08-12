53cf3f31cce76fae000c26cfbf7e415e5aeac4d5
Sarah Goodell <sarahlgoodell1@gmail.com>
2023-08-11 22:06:28 -0400

    Cleaned up comments in JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,18 +31,15 @@ submitButton.addEventListener('click', function () {
 });
 
 // DRINK SECTION
+
 // API Call for a random cocktail
 // www.thecocktaildb.com/api/json/v1/1/random.php
 // API Call for filter by non Alcoholic Drink
 // www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic
 
-// Need to toggle alcoholic vs non alcoholic drinks
-// use if else statement
-// if drink has the value of `Alcoholic`use the aloholic drink function
-// then return the result
-// else use the filter by  nonalcoholic and use math random
-// then return the result
-// 
+// TODO
+// Use data from local storage to access the toggle switch data and run either getRandomAlcoholicDrink(); or getRandomNonAlcoholicDrink();
+
 getDrink();
 
 function getDrink() {
