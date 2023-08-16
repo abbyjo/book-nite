5fef0a35c0865599d045d52c9447816f324bac93
abbyjo <abbyjo225@gmail.com>
2023-08-15 21:29:08 -0400

    Edited video width

--- assets/js/script.js
+++ assets/js/script.js
@@ -91,11 +91,11 @@ function getBooks(){
 // MUSIC SECTION ---------------------------------------->
 var music = [
  
-     '<iframe width="560" height="315" src="https://www.youtube.com/embed/NPyiLkNf_0c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
-     '<iframe width="560" height="315" src="https://www.youtube.com/embed/gGOpElxqlQw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
-     '<iframe width="560" height="315" src="https://www.youtube.com/embed/o26qoCYLdS8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
-     '<iframe width="560" height="315" src="https://www.youtube.com/embed/3VNm3rGXPXs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
-     '<iframe width="560" height="315" src="https://www.youtube.com/embed/htnobkrtDoo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'
+     '<iframe width="100%" height="315" src="https://www.youtube.com/embed/NPyiLkNf_0c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+     '<iframe width="100%" height="315" src="https://www.youtube.com/embed/gGOpElxqlQw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+     '<iframe width="100%" height="315" src="https://www.youtube.com/embed/o26qoCYLdS8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+     '<iframe width="100%" height="315" src="https://www.youtube.com/embed/3VNm3rGXPXs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+     '<iframe width="100%" height="315" src="https://www.youtube.com/embed/htnobkrtDoo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'
 ]
 
 function getMusic() {
